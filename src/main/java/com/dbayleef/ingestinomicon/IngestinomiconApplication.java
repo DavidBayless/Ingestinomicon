48b5f3af87b9fde44cb3b0ebfd40a626bd3dcc20
David Bayless <davidbayless@Davids-MacBook-Air.local>
2020-09-16 15:45:30 -0700

    init commit

---
+++ src/main/java/com/dbayleef/ingestinomicon/IngestinomiconApplication.java
@@ -0,0 +1,13 @@
+package com.dbayleef.ingestinomicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestinomiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestinomiconApplication.class, args);
+	}
+
+}
